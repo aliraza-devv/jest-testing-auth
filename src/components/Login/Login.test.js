5c2b46d957249a9cb817bac3a2f5c314e5bb0a45
Ali Raza <aliraza.dev.web@gmail.com>
2022-08-19 18:10:44 +0500

    user testing

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -6,7 +6,7 @@ jest.mock("axios", () => ({
 
   default: {
     get: () => ({
-      data: { id: 1, name: "john" },
+      data: { id: 1, name: "John" },
     }),
   },
 }));
@@ -115,4 +115,21 @@ it("loading should not be rendered after fetching", async () => {
   fireEvent.change(passwordInputEl, { target: { value: testValue } });
   fireEvent.click(buttonEl);
   await waitFor(() => expect(buttonEl).not.toHaveTextContent(/please wait/i))
+});
+
+it("user should be rendered after fetching", async () => {
+  render(<Login />);
+  const buttonEl = screen.getByRole("button");
+  const usernameInputEl = screen.getByPlaceholderText(/username/i);
+  const passwordInputEl = screen.getByPlaceholderText(/password/i);
+
+  const testValue = "test";
+
+  fireEvent.change(usernameInputEl, { target: { value: testValue } });
+  fireEvent.change(passwordInputEl, { target: { value: testValue } });
+  fireEvent.click(buttonEl);
+
+  const userItem = await screen.findByText("John")
+
+  expect(userItem).toBeInTheDocument();
 });
